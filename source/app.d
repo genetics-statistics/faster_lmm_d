68554f13b2f2b95126d266e52034eddde5b39523
prasun <prasunanand.bitsp@gmail.com>
2016-12-02 05:51:04 +0530

    rqtlreader init

--- source/app.d
+++ source/app.d
@@ -1,6 +1,31 @@
 import std.stdio;
+import std.string;
+import std.array;
+import dyaml.all;
 
-void main()
+Node control(string fn){
+  Node root = Loader(fn).load();
+  return root;
+}
+
+int kinship(string fn){
+  //K1 = []
+  writeln(fn);
+  string input = cast(string)std.file.read(fn);
+  foreach (line; input.split("\n"))
+    writeln(line);
+  return 1;
+}
+
+
+void main(string[] argv)
 {
-	writeln("Edit source/app.d to start your project.");
+
+  Node root = control("./source/input.yaml");
+  //writeln("The answer is ", root["crosstype"].as!int);
+
+  kinship("./data/rqtl/iron_covar.csv");
+
+  //Dump the loaded document to output.yaml.
+  Dumper("output.yaml").dump(root);
 }
