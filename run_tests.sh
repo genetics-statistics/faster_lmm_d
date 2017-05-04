1332721e86ac63b2f53a3ab9371a9b90037398f2
Pjotr Prins <pjotr.public01@thebird.nl>
2017-05-04 09:05:58 +0000

    Add small_na test and some output

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
-echo "Running small"
+echo "Running small test"
 ./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/small.pheno --geno=data/small.geno --cmd=run $*
-if [ $? -ne 0 ]; then echo "ERR1" ; exit 1 ; fi
+if [ $? -ne 0 ]; then echo "ERR1a small test failed" ; exit 1 ; fi
+
+echo "Running small_na test"
+./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/small_na.pheno --geno=data/small_na.geno --cmd=run $*
+if [ $? -ne 0 ]; then echo "ERR1b small_na test failed" ; exit 1 ; fi
+
+echo "Running 104617_at"
 ./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/genenetwork/104617_at.json --geno=data/genenetwork/BXD.csv --cmd=rqtl $*
-if [ $? -ne 0 ]; then echo "ERR2" ; exit 1 ; fi
+if [ $? -ne 0 ]; then echo "ERR2 104617_at test failed" ; exit 1 ; fi
+
 echo "Running test8000"
-./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/test8000.pheno --geno=data/test8000.geno --cmd=run $*
-if [ $? -ne 0 ]; then echo "ERR3" ; fi
+time ./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/test8000.pheno --geno=data/test8000.geno --cmd=run $*
+if [ $? -ne 0 ]; then echo "ERR3 test8000 failed" ; fi
