07f7255b75ad1c456395ea9f84d3f2abd5168d97
Pjotr Prins <pjotr.public01@thebird.nl>
2017-05-26 06:14:39 -0500

    Dmatrix.validate

--- source/faster_lmm_d/optmatrix.d
+++ source/faster_lmm_d/optmatrix.d
@@ -30,13 +30,7 @@ version(CUDA) {
   DMatrix matrix_mult(const DMatrix lha,const DMatrix rha) {
     auto cuda_result = cuda_matrix_mult(lha,rha);
     auto cpu_result  = cpu_matrix_mult(lha,rha);
-    assert(cuda_result.rows == cpu_result.rows, "CUDA rows mismatch, expected "~to!string(cpu_result.rows)~" but got "~to!string(cuda_result.rows));
-    assert(cuda_result.rows == cpu_result.cols, "CUDA rows mismatch, expected "~to!string(cpu_result.cols)~" but got "~to!string(cuda_result.cols));
-    stderr.write("CPU result:");
-    pretty_print(cpu_result);
-    stderr.write("CUDA result:");
-    pretty_print(cuda_result);
-    assert(cuda_result.sum == cpu_result.sum, "CUDA result mismatch, expected "~to!string(cpu_result.sum)~" but got "~to!string(cuda_result.sum));
+    cuda_result.validate(cpu_result);
     return cuda_result;
   }
 } else {
