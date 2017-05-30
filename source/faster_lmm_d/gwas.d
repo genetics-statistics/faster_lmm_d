02f06432ad882c3d02e877cf50de654ae5fdb2a3
Pjotr Prins <pjotr.public01@thebird.nl>
2017-05-30 04:48:36 -0500

    Moved gwas into gwas.d

--- source/faster_lmm_d/gwas.d
+++ source/faster_lmm_d/gwas.d
@@ -11,6 +11,8 @@ import std.experimental.logger;
 import std.typecons;
 
 import faster_lmm_d.dmatrix;
+import faster_lmm_d.kinship;
+import faster_lmm_d.phenotype;
 import faster_lmm_d.lmm2;
 import faster_lmm_d.memory;
 import faster_lmm_d.optmatrix;
@@ -69,4 +71,24 @@ auto gwas(immutable double[] Y, const DMatrix G, const DMatrix K, const bool rem
   }
 
   return Tuple!(double[], double[], double[])(ts, ps, lod);
+}
+
+
+auto run_gwas(immutable m_items n, immutable m_items m, DMatrix k, immutable double[] y, const DMatrix geno) {
+  trace("run_gwas");
+  trace("pheno ", y.length," ", y[0..4]);
+  trace(geno.shape,m);
+  check_memory("before run_gwas");
+  assert(y.length == n);
+  assert(geno.m_geno == m);
+
+  PhenoStruct pheno = remove_missing(n,y);
+
+  auto geno2 = remove_cols(geno,pheno.keep);
+  DMatrix G = normalize_along_row(geno2);
+  trace("run_other_new genotype_matrix: ", G.shape);
+  DMatrix K = kinship_full(G);
+  trace("kinship_matrix.shape: ", K.shape);
+
+  return gwas(pheno.Y, G, K, true, false, true);
 }
