5bfa0ebcc9f1ee836bb209c8760bb92844f6c916
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-07-14 15:05:05 +0530

    remove control file when cmd!=rqtl

--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 echo "Running small test"
-./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/small.pheno --geno=data/small.geno --test-kinship=true --cmd=run $*
+./build/faster_lmm_d --pheno=data/small.pheno --geno=data/small.geno --test-kinship=true --cmd=run $*
 if [ $? -ne 0 ]; then echo "ERR1a small test failed" ; exit 1 ; fi
 
 echo "Running small_na test"
-./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/small_na.pheno --geno=data/small_na.geno --test-kinship=true --cmd=run $*
+./build/faster_lmm_d --pheno=data/small_na.pheno --geno=data/small_na.geno --test-kinship=true --cmd=run $*
 if [ $? -ne 0 ]; then echo "ERR1b small_na test failed" ; exit 1 ; fi
 
 echo "iron"
@@ -21,6 +21,6 @@ echo "Running 104617_at"
 if [ $? -ne 0 ]; then echo "ERR2b 104617_at test failed" ; exit 1 ; fi
 
 echo "Running test8000"
-time ./build/faster_lmm_d --control=data/genenetwork/BXD.json --pheno=data/test8000.pheno --geno=data/test8000.geno --test-kinship=true --cmd=run $*
+time ./build/faster_lmm_d --pheno=data/test8000.pheno --geno=data/test8000.geno --test-kinship=true --cmd=run $*
 if [ $? -ne 0 ]; then echo "ERR3 test8000 failed" ; fi
 
