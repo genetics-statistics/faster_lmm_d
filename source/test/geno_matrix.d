f97adaa0550ab45c02b7174cb0e4bbe3ab887beb
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-07-16 18:30:03 +0530

    enforce shape for geno_matrix tests

--- source/test/geno_matrix.d
+++ source/test/geno_matrix.d
@@ -18,36 +18,42 @@ void check_geno_matrix(DMatrix  geno_matrix, string geno_fn){
   double gl = geno_matrix.elements[$-1];
   if(geno_fn == "data/small.geno"){
     info("Validating results for ", geno_fn);
+    enforce(geno_matrix.shape == [6,5]);
     enforce(modDiff(g1, 1) < 0.001);
     enforce(modDiff(g2, 1) < 0.001);
     enforce(modDiff(gl, 1) < 0.001);
   }
   if(geno_fn == "data/small_na.geno"){
     info("Validating results for ", geno_fn);
+    enforce(geno_matrix.shape == [6,4]);
     enforce(modDiff(g1, 1) < 0.001);
     enforce(modDiff(g2, 1) < 0.001);
     enforce(modDiff(gl, 0) < 0.001);
   }
   if(geno_fn == "data/genenetworg/BXD.csv"){
     info("Validating results for ", geno_fn);
+    enforce(geno_matrix.shape == [66,284]);
     enforce(modDiff(g1, 1) < 0.001);
     enforce(isNaN(g2));
     enforce(modDiff(gl, 0.5) < 0.001);
   }
   if(geno_fn == "data/rqtl/recla_geno.csv"){
     info("Validating results for ", geno_fn);
+    enforce(geno_matrix.shape == [6370,261]);
     enforce(modDiff(g1, 0) < 0.001);
     enforce(modDiff(g2, 0.5 ) < 0.001);
     enforce(modDiff(gl, 0) < 0.001);
   }
   if(geno_fn == "data/rqtl/iron_geno.csv"){
     info("Validating results for ", geno_fn);
+    enforce(geno_matrix.shape == [66,284]);
     enforce(modDiff(g1, 1) < 0.001);
     enforce(isNaN(g2));
     enforce(modDiff(gl, 0.5) < 0.001);
   }
   if(geno_fn == "data/test8000.geno"){
     info("Validating results for ", geno_fn);
+    enforce(geno_matrix.shape == [8000,1219]);
     enforce(modDiff(g1, 0) < 0.001);
     enforce(modDiff(g2, 0) < 0.001);
     enforce(modDiff(gl, 0) < 0.001);
