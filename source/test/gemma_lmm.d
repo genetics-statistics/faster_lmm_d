3a349f7053cbf7b598a827c6a22cb457b99cb500
Prasun Anand <prasunanand.bitsp@gmail.com>
2017-10-11 21:28:32 +0530

    init test file

---
+++ source/test/gemma_lmm.d
@@ -0,0 +1,33 @@
+module test.gemma_lmm;
+
+import std.exception;
+import std.experimental.logger;
+import std.math : round, isNaN;
+import std.stdio;
+
+import faster_lmm_d.dmatrix;
+import faster_lmm_d.helpers : modDiff, sum;
+
+void check_trace_G(double a){
+  
+}
+
+void check_logH0(double h0){
+
+}
+
+void check_mle(){
+
+}
+
+void check_beta(){
+
+}
+
+void check_sigma(){
+
+}
+
+void checkPve(){
+
+}
