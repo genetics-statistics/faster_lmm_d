ea2da1330b709e98f050cb4c762054bc843653f5
prasun <prasunanand.bitsp@gmail.com>
2018-02-05 17:00:41 +0530

    add test data files for MVLMM

--- gemma_tests.sh
+++ gemma_tests.sh
@@ -24,3 +24,9 @@ then
 echo "testBXDMultivariateLinearMixedModel()"
 ./build/faster_lmm_d --pheno=data/gemma/BXD.pheno --geno=data/gemma/BXD_geno.txt.gz --kinship=data/gemma/BXD.kinship --covar=data/gemma/BXD_covariates2.txt --indicator_idv=data/gemma/BXD_indicator_idv.txt --indicator_snp=data/gemma/BXD_indicator_snp.txt --test-kinship=true --test-name=BXD --cmd=run
 fi
+
+if [ $1 -eq 4 ];
+then
+echo "testBXDMultivariateLinearMixedModel()"
+./build/faster_lmm_d -bfile=data/gemma//HLC -pheno=data/gemma/HLC.simu.pheno.txt -kinship=PlinkStandardRelatednessMatrixK.txt --lmm=1 --maf=0.1 --n=12 3 15 --c=$datadir/HLC_covariates.txt
+fi
