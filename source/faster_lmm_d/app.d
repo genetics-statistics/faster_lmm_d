1a9e68614b977cf2069e5bd208f8096c8c48b199
Prasun Anand <prasunanand.bitsp@gmail.com>
2018-06-29 14:25:55 +0530

    set up LM tests and declare types as const

--- source/faster_lmm_d/app.d
+++ source/faster_lmm_d/app.d
@@ -193,7 +193,7 @@ void main(string[] args)
       bslmm_run(option_geno, option_pheno);
     }
     else if(cmd == "lm"){
-      lm_run(option_anno, option_kinship, option_pheno, option_covar, option_geno, option_bfile, option_lm);
+      lm_run(option_anno, option_pheno, option_covar, option_geno, option_bfile, option_lm);
     }
     else{
       batch_run(option_kinship, option_pheno, option_covar, option_geno, option_indicator_idv,
